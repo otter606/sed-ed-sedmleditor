f36772d19d371c5bf93b666f89f3f62d43d25513
ra22597@gmail.com <ra22597@gmail.com@2407fa09-a639-c399-f4f8-ff75025acbf0>
2012-03-30 22:10:01 +0000

    use jlibsedm lxml utils now

--- org.sedml.jlibsedml.editor/trunk/test/org/sedml/jlibsedml/xmlutils/TestUtils.java
+++ org.sedml.jlibsedml.editor/trunk/test/org/sedml/jlibsedml/xmlutils/TestUtils.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 import org.jdom.Document;
 import org.jdom.JDOMException;
+import org.jlibsedml.extensions.XMLUtils;
 
 public class TestUtils {
 	static Document getDoc(String in) throws JDOMException, IOException {
